933b45bcb7460b2d5be25b72cd3b7f35c8717992
AndreaGhizzoni <andrea.ghz@gmail.com>
2017-11-13 11:31:14 +0100

    added new template for bash script

--- vim/skeletons/template.bash
+++ vim/skeletons/template.bash
@@ -5,49 +5,59 @@ GREEN='\033[0;32m'
 RED='\033[0;31m'
 NC='\033[0m' # No color
 
+# print $1 in green
+function log () {
+    echo -e -n "${GREEN}"; echo -n "$1"; echo -e "${NC}"
+}
+
+# print $1 in red
+function logError () {
+    echo -e -n "${RED}"; echo -n "$1"; echo -e "${NC}"
+}
+
 # usage and exit function.
-usageAndExit(){
+function usageAndExit () {
     echo -e "Usage: $0"
     echo -e "Example: $0"
     exit 1
 }
 
-# check if this script is running as root.
+# check if this script is running with EUID==0 (root)
 # comment the following statement if not required
-if [ $UID -ne 0 ]; then
-    echo -e "${RED}$0 must be run as root.${NC}"
+if [ "$EUID" -ne 0 ]; then
+    logError "$0 must be run as root."
     usageAndExit
 fi
 
-# check if correct number of parameters are passed to this script.
-# change the following number: 1 == no parameters, 2 == 1 parameters ecc.
-if [ "$#" -ne 1 ]; then
-    echo -e "${RED}Parameters missing.${NC}"
+# check if correct number of arguments are passed to this script.
+# 0 == no parameters, 1 == 1 argument, 2 == 2 arguments [...]
+if [ "$#" -ne 0 ]; then
+    logError "Script arguments are missing!"
     usageAndExit
 fi
 
-# insert the required packages, space separated.
+# check if required packages are installed.
 # if no dependencies required for this script, just skip it without modify.
+# insert the required packages, space separated.
 dep_req=( )
-# check if required packages are installed
-dep_not_found=( )
+dep_not_found=( ) # DO NOT EDIT THIS ARRAY
 i=0
 for dep in "${dep_req[@]}"
 do
-    q=$(dpkg-query -W -f='${Status}' ${dep} 2>/dev/null | grep -c "ok installed")
-    if [ ${q} -eq 0 ]; then
+    q=$(dpkg-query -W -f='${Status}' "$dep" 2>/dev/null | grep -c "ok installed")
+    if [ "$q" -eq 0 ]; then
         dep_not_found[$i]=${dep}
         ((i++))
     fi
 done
 if [ ${i} -ne 0 ]; then
-    echo -e "${RED}Abort: Dependency package/s not found: ${NC}"
-    printf '%s ' "${dep_not_found[@]}"
+    logError "Abort: Dependency package/s not found:"
+    echo -n "${dep_not_found[@]}"
     echo ""
     exit 1
 fi
 
 # script logic start here
-echo -e "${GREEN} ${NC}"
+log "Script Starting!"
 
-echo -e "${GREEN}Done!${NC}"
+log "Done!"
