16fb67569006366632c93e759ceac43016c849c1
AndreaGhizzoni <andrea.ghz@gmail.com>
2017-12-06 15:50:11 +0100

    added new i3lock

--- i3/common/i3exit.sh
+++ i3/common/i3exit.sh
@@ -1,6 +1,46 @@
 #!/bin/bash
+
 function lock {
-    i3lock --ignore-empty-password -c 202020
+   GREY='#202020ff'
+   CLEAR='#00000000'
+   GREEN='#00ff00ff'
+   WHITE='#ffffffff'
+   RED='#ff0000bb'  # wrong
+   BLUE='#0000ffff'  # verifying
+
+   # using new i3lock: https://github.com/chrjguill/i3lock-color
+   i3lock \
+       -c 202020 \
+       --insidevercolor=$CLEAR \
+       --ringvercolor=$BLUE \
+       \
+       --insidewrongcolor=$CLEAR \
+       --ringwrongcolor=$RED \
+       \
+       --insidecolor=$GREY \
+       --linecolor=$GREY \
+       --ringcolor=$GREEN \
+       --separatorcolor=$GREEN \
+       \
+       --textcolor=$WHITE \
+       --timecolor=$WHITE \
+       --datecolor=$WHITE \
+       --layoutcolor=$WHITE \
+       --keyhlcolor=$RED \
+       --bshlcolor=$RED \
+       \
+       --radius=120 \
+       --clock \
+       --indicator \
+       --wrongtext="Nope!" \
+       --timestr="%H:%M:%S" \
+       --datestr="%A, %m %Y" \
+       #--veriftext="Drinking verification can..." \
+       # --textsize=20
+       # --modsize=10
+       # --timefont=comic-sans
+       # --datefont=monofur
+       # etc
 }
 
 case "$1" in
