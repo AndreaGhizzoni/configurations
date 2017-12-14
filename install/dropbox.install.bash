eebbe76262a70a849a86d993e9cd8c9428ac9b01
AndreaGhizzoni <andrea.ghz@gmail.com>
2017-12-14 17:57:53 +0100

    apply new bash template to all installation script

--- install/dropbox.install.bash
+++ install/dropbox.install.bash
@@ -5,40 +5,76 @@ GREEN='\033[0;32m'
 RED='\033[0;31m'
 NC='\033[0m' # No color
 
-# print $1 in green
+## print $1 in green.
+# $1: something to print in GREEN on stdout
 function log () {
     echo -e -n "${GREEN}"; echo -n "$1"; echo -e "${NC}"
 }
 
-# print $1 in red
+## print $1 in red.
+# $1: something to print in RED on stdout
 function logError () {
     echo -e -n "${RED}"; echo -n "$1"; echo -e "${NC}"
 }
 
-# usage and exit function.
+## print usage and exit.
 function usageAndExit () {
     echo -e "Usage: $0"
     #echo -e "Example: $0"
     exit 1
 }
 
-if [ "$1" = "-h" ] || [ "$1" = "--help"  ]; then
+## print some help string and call usageAndExit
+function printHelp () {
+    echo "dropbox installation script."
     usageAndExit
-fi
+}
 
 # check if this script is running with EUID==0 (root)
 # comment the following statement if not required
 if [ "$EUID" -eq 0 ]; then
-    logError "$0 it is not necessary to run this script as root"
+    #logError "$0 must be run as root."
+    logError "$0 must be executed a non-root user."
     usageAndExit
 fi
 
 # check if correct number of arguments are passed to this script.
 # 0 == no parameters, 1 == 1 argument, 2 == 2 arguments [...]
-if [ "$#" -ne 0 ]; then
-    logError "Script arguments are missing!"
-    usageAndExit
+#if [ "$#" -ne 0 ]; then
+#    logError "Script arguments are missing!"
+#    usageAndExit
+#fi
+
+getopt --test > /dev/null
+if [[ $? -ne 4 ]]; then
+    echo "I’m sorry, $(getopt --test) failed in this environment."
+    exit 1
 fi
+
+OPTIONS=h
+LONGOPTIONS=help
+PARSED=$(getopt --options=$OPTIONS \
+                --longoptions=$LONGOPTIONS \
+                --name "$0" -- "$@")
+eval set -- "$PARSED"
+
+# now enjoy the options in order and nicely split until we see --
+while true; do
+    case "$1" in
+        -h|--help)
+            printHelp
+            ;;
+        # this case is required to handle --arg
+        --)
+            shift
+            break
+            ;;
+        *)
+            echo "Programming error"
+            exit 3
+            ;;
+    esac
+done
 
 # check if required packages are installed.
 # if no dependencies required for this script, just skip it without modify.
