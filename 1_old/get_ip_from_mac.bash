8484070d87341a2d1629bb966f48628210663818
AndreaGhizzoni <andrea.ghz@gmail.com>
2018-01-17 11:44:49 +0100

    removed old directory

--- 1_old/get_ip_from_mac.bash
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-# Here nmap will do a ping scan and populate your arp cache. Once the scan is
-# done, the arp command can be used to print the arp table and then you pull out
-# the IP address with grep/awk. You could try replacing nmap with a broadcast
-# ping, but that probably isn't as reliable. 
-
-nmap -sP 192.168.2.0/24 >/dev/null && \
-    arp -an | grep $1 | awk '{print $2}' | sed 's/[()]//g'
