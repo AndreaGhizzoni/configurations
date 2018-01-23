f628110db9f594494ea2fd0a38a62d4b21137f1e
AndreaGhizzoni <andrea.ghz@gmail.com>
2018-01-23 16:06:07 +0100

    added X11 script

---
+++ X11/seedbox-X11.sh
@@ -0,0 +1,21 @@
+#! /bin/sh
+
+u=pi
+m=seedbox.local
+
+# lancia Xephyr su display :1
+Xephyr :1 -ac -nolisten tcp -screen 1440x900 &
+pid_xephyr=$!
+
+# lancia lxde via ssh su display :1
+exit_status=0
+DISPLAY=:1 ssh "$u@$m" -X -C -- startlxde ||
+   exit_status=$?
+
+# termina Xephyr quando lxde termina
+kill $pid_xephyr 2> /dev/null
+{ sleep 5; kill -9 $pid_xephyr 2> /dev/null ; }&
+wait $pid_xephyr 2> /dev/null
+
+# esce con l'exit status di lxde
+exit $exit_status
